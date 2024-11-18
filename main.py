19a04187992562344cbed29558f555a3754bb513
hhayaan <hayaan.ahmad@torontomu.ca>
2024-11-18 07:00:18 -0500

    Pushed the whole dataset lol

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world!")
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolo11n.yaml") #Build YOLOv11 nano from scratch
+
+# Train the model
+train_results = model.train(
+    data="coco8.yaml",  # path to dataset YAML
+    epochs=100,  # number of training epochs
+    imgsz=640,  # training image size
+)
