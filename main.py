ba9920f1157564a543cf70ba700cddb6963dad8e
hhayaan <hayaan.ahmad@torontomu.ca>
2024-11-18 07:34:49 -0500

    :zap:

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ model = YOLO("yolo11n.yaml") #Build YOLOv11 nano from scratch
 
 # Train the model
 results = model.train(
-    data="config.yaml",  # path to dataset YAML
-    epochs=1,  # number of training epochs
-    imgsz=640,  # training image size
-    device=0,
+    data="config.yaml",  #Path to dataset YAML
+    epochs=1,  #Number of training epochs
+    imgsz=640,  #Training image size
+    device=0, #Single GPU (device=0), multiple GPUs (device=0,1), CPU (device=cpu)
 )
