7f80dbccd0f01aaac8de716156075d4ba21c2a1d
hhayaan <hayaan.ahmad@torontomu.ca>
2024-11-18 07:28:39 -0500

    :fire:

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.yaml") #Build YOLOv11 nano from scratch
 
 # Train the model
-train_results = model.train(
-    data="coco8.yaml",  # path to dataset YAML
-    epochs=100,  # number of training epochs
+results = model.train(
+    data="config.yaml",  # path to dataset YAML
+    epochs=1,  # number of training epochs
     imgsz=640,  # training image size
+    device=0,
 )
